ef583f2484eb14a8f57eebdf2b8e320b89d265da
chouchouji <1305974212@qq.com>
2024-11-03 13:43:57 +0800

    chore: add generate script

---
+++ scripts/generate.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { jsdocToMD } from '../dist/index.js'
+
+function generateMD() {
+  const examplePath = fileURLToPath(new URL('../example', import.meta.url))
+  const docsPath = fileURLToPath(new URL('../docs', import.meta.url))
+  const exampleFiles = fs.readdirSync(examplePath)
+
+  exampleFiles.forEach((file) => {
+    const [fileName, extname] = file.split('.')
+    const input = fs.readFileSync(`${examplePath}/${file}`, 'utf-8')
+
+    const output = jsdocToMD({ input, extname })
+
+    if (!fs.existsSync(docsPath)) {
+      fs.mkdirSync(docsPath)
+    }
+    fs.writeFileSync(`${docsPath}/${fileName}.md`, output)
+  })
+}
+
+generateMD()
